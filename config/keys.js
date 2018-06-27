d5948591e79abf19c584338b7a6a14392b229476
Captain Storm <CaptainStorm21@users.noreply.github.com>
2018-06-27 02:38:15 -0700

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://phoenix:phoenix2012@ds127429.mlab.com:27429/phoenix2012'
+};
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+//create schema
+//building data
+const UserSchema = new Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
