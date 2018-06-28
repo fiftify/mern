30e3ab19fb0cfde7de2905625568427e87b88e1e
Shubham Tiwari <websbiz0000@gmail.com>
2018-06-28 11:03:31 +0000

    delete keys

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    //mongoURI: 'mongodb://masterAcc:account1@ds127429.mlab.com:27429/phoenix2012',
-    mongoURI: 'mongodb://localhost/phoenix2012',
-    secretOrKey: 'secret'
-};
